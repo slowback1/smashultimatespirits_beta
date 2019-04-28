b96f5a1a8328fb8d5567fd5a33ac5afcdcf48b21
Andrew Wobeck <dwobeck@gmail.com>
2019-04-28 13:20:52 +0000

    SCSS structure started; swipe functionality added

--- templates/header.php
+++ templates/header.php
@@ -27,7 +27,7 @@
         </div>
     </nav>
     <div class="sidebar" id="sidebar">
-
+        <!-- things need to go here -->
     </div>
 <script>
     //functions that go here:
@@ -53,9 +53,59 @@
         }
     }
     document.getElementById('hamburgerBtn').addEventListener('click', toggleSidebar(), false);
+
+    class SwipeEventDispatcher {
+        constructor(element, options = {}) {
+            this.evtMap = {
+                SWIPE_LEFT: [],
+                SWIPE_UP: [],
+                SWIPE_DOWN: [],
+                SWIPE_RIGHT: []
+            };
+            this.xDown = null;
+            this.yDown = null;
+            this.options = Object.assign({triggerPercent: 0.3}, options);
+
+            element.addEventListener('touchstart', evt => this.handleTouchStart(evt), false);
+            element.addEventListener('touchend', evt => this.handleTouchEnd(evt), false);
+
+            on(evt, cb) {
+                this.evtMap[evt].push(cb);
+            }
+            off(evt, lcb) {
+                this.evtMap[evt].map(handler => handler(data));
+            }
+            handleTouchStart(evt) {
+                this.xDown = evt.touches[0].clientX;
+                this.yDown = evt.touches[0].clientY;
+            }
+            handleTouchEnd(evt) {
+                const deltaX = evt.changedTouches[0].clientX - this.xDown;
+                const deltaY = evt.changedTouches[0].clientY - this.yDown;
+                const distMoved = Math.abs(Math.abs(deltaX) > Math.abs(deltaY) ? deltaX : deltaY);
+                const activePct = distMoved / this.element.offsetWidth;
+
+                if(activePct > this.options.triggerPercent) {
+                    if(Math.abs(deltaX) > Math.abs(deltaY)) {
+                        deltaX < 0 ? this.trigger('SWIPE_LEFT') : this.trigger('SWIPE_RIGHT');
+                    } else {
+                        deltaY < 0 ? this.trigger('SWIPE_DOWN') : this.trigger('SWIPE_UP');
+                    }
+                }
+            }
+        }
+    }
+
+    const dispatcher = new SwipeEventDispatcher(document.getElementById('main')); //needs a better home than the entire main div  Possible solution: make an invisible div that sits on the left half of the screen that anchors this swipe handler
+    dispatcher.on('SWIPE_RIGHT', () => {if(!isOpen){toggleSideBar()}});
+    dispatcher.on('SWIPE_LEFT', () => {if(isOpen){toggleSideBar()}});
+
+
     function findAutoResult() {
 
     }
+
+
     function searchResults() {
 
     }
